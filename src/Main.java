a2be621670677c33ec059f06206ceb84d6d5a101
Neto Test <viktoriyvl@mail.ru>
2026-05-11 19:59:51 +0300

    initiai commit

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,14 @@
 public class Main {
     public static void main(String[] args) {
 
-        int ticketPrice = 13676;// Cтоимость билета
+        int ticketPrice = 13_676;// Cтоимость билета
 
         // Рассчитываем количество миль: за каждые 20 рублей — 1 миля
         // Дробные мили не начисляются, поэтому используем целочисленное деление
         int miles = ticketPrice / 20;
 
         // Результат
-        System.out.println("Стоимость билета:" + ticketPrice + "рублей");
-        System.out.println("Начислено миль:" + miles);
+        System.out.println("ticketPrice:" + ticketPrice + "rub");
+        System.out.println("accrued miles:" + miles);
     }
 }
